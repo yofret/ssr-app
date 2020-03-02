f12d033db3f0977b408803530d2cdc37ea7b8933
Yofret Rios <yofret.rios@jobsity.com>
2020-03-02 14:31:46 -0500

    feat: add JSX on the server

--- webpack.server.js
+++ webpack.server.js
@@ -1,3 +1,31 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+  // Inform webpack we are bulding a bundle
+  // for nodeJS, rather than for the browser
+  target: 'node',
+  // Tell Webpack the root file of our server
+  entry: './src/index.js',
+  // Tell Webpack where to put the output file that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  // Tell Webpack to run bable on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } }],
+          ]
+        }
+      }
+    ]
+  }
 };
